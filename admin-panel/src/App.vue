a365248693868e2001daf5cc03a159839e29b646
kebasyaty <kebasyaty@gmail.com>
2021-01-19 10:08:27 +0200

    Update of buttons in footer

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -32,21 +32,21 @@
       <v-card flat tile width="100%">
         <v-divider></v-divider>
         <v-footer color="transparent" class="pb-0">
-          <v-btn rounded small depressed color="red">
+          <v-btn rounded small depressed color="error">
             <v-icon color="white">mdi-close-thick</v-icon>
           </v-btn>
           <v-spacer></v-spacer>
-          <v-btn rounded small depressed color="green">
+          <v-btn rounded small depressed color="success">
             <v-icon color="white">mdi-content-save</v-icon>
             <v-icon color="white">mdi-ampersand</v-icon>
             <v-icon color="white">mdi-file-outline</v-icon>
           </v-btn>
-          <v-btn rounded small depressed color="orange" class="mx-4">
+          <v-btn rounded small depressed color="warning" class="mx-4">
             <v-icon color="white">mdi-content-save</v-icon>
             <v-icon color="white">mdi-ampersand</v-icon>
             <v-icon color="white">mdi-file-document-edit-outline</v-icon>
           </v-btn>
-          <v-btn rounded small depressed color="blue">
+          <v-btn rounded small depressed color="primary">
             <v-icon color="white">mdi-content-save</v-icon>
           </v-btn>
         </v-footer>
