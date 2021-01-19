110d21be4e8d0be19f9931d4bff8848fd8d9ce7b
kebasyaty <kebasyaty@gmail.com>
2021-01-19 11:25:40 +0200

    Add tooltip for buttons in footer

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -22,7 +22,7 @@
             <v-icon>mdi-home</v-icon>
           </v-btn>
         </template>
-        <span>Home page.</span>
+        <span>Home page</span>
       </v-tooltip>
       <!-- Logout button. -->
       <v-tooltip right>
@@ -31,7 +31,7 @@
             <v-icon>mdi-logout</v-icon>
           </v-btn>
         </template>
-        <span>Logout.</span>
+        <span>Logout</span>
       </v-tooltip>
       <v-spacer></v-spacer>
       <!-- Light or dark theme button. -->
@@ -41,7 +41,7 @@
             <v-icon>mdi-brightness-4</v-icon>
           </v-btn>
         </template>
-        <span>Light or Dark theme.</span>
+        <span>Light or Dark theme</span>
       </v-tooltip>
     </v-app-bar>
 
@@ -58,26 +58,46 @@
         <v-divider></v-divider>
         <v-footer color="transparent" class="pb-0">
           <!-- Delete button. -->
-          <v-btn rounded small depressed color="error">
-            <v-icon color="white">mdi-close-thick</v-icon>
-          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn rounded small depressed color="error" v-bind="attrs" v-on="on">
+                <v-icon color="white">mdi-close-thick</v-icon>
+              </v-btn>
+            </template>
+            <span>Delete document</span>
+          </v-tooltip>
           <v-spacer></v-spacer>
           <!-- Save button and create a new document. -->
-          <v-btn rounded small depressed color="success">
-            <v-icon color="white">mdi-content-save</v-icon>
-            <v-icon color="white">mdi-ampersand</v-icon>
-            <v-icon color="white">mdi-file-outline</v-icon>
-          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn rounded small depressed color="success" v-bind="attrs" v-on="on">
+                <v-icon color="white">mdi-content-save</v-icon>
+                <v-icon color="white">mdi-ampersand</v-icon>
+                <v-icon color="white">mdi-file-outline</v-icon>
+              </v-btn>
+            </template>
+            <span>Save and create a new document</span>
+          </v-tooltip>
           <!-- Save button and continue editing the document. -->
-          <v-btn rounded small depressed color="warning" class="mx-4">
-            <v-icon color="white">mdi-content-save</v-icon>
-            <v-icon color="white">mdi-ampersand</v-icon>
-            <v-icon color="white">mdi-file-document-edit-outline</v-icon>
-          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn rounded small depressed color="warning" class="mx-4" v-bind="attrs" v-on="on">
+                <v-icon color="white">mdi-content-save</v-icon>
+                <v-icon color="white">mdi-ampersand</v-icon>
+                <v-icon color="white">mdi-file-document-edit-outline</v-icon>
+              </v-btn>
+            </template>
+            <span>Save and continue editing the document</span>
+          </v-tooltip>
           <!-- Save button. -->
-          <v-btn rounded small depressed color="primary">
-            <v-icon color="white">mdi-content-save</v-icon>
-          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn rounded small depressed color="primary" v-bind="attrs" v-on="on">
+                <v-icon color="white">mdi-content-save</v-icon>
+              </v-btn>
+            </template>
+            <span>Save document</span>
+          </v-tooltip>
         </v-footer>
       </v-card>
     </v-footer>
