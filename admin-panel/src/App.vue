0f7019007162f4ea0099fe833ad758c923fb1521
kebasyaty <kebasyaty@gmail.com>
2021-01-19 11:12:25 +0200

    Add comments

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -57,20 +57,24 @@
       <v-card flat tile width="100%">
         <v-divider></v-divider>
         <v-footer color="transparent" class="pb-0">
+          <!-- Delete button. -->
           <v-btn rounded small depressed color="error">
             <v-icon color="white">mdi-close-thick</v-icon>
           </v-btn>
           <v-spacer></v-spacer>
+          <!-- Save button and create a new document. -->
           <v-btn rounded small depressed color="success">
             <v-icon color="white">mdi-content-save</v-icon>
             <v-icon color="white">mdi-ampersand</v-icon>
             <v-icon color="white">mdi-file-outline</v-icon>
           </v-btn>
+          <!-- Save button and continue editing the document. -->
           <v-btn rounded small depressed color="warning" class="mx-4">
             <v-icon color="white">mdi-content-save</v-icon>
             <v-icon color="white">mdi-ampersand</v-icon>
             <v-icon color="white">mdi-file-document-edit-outline</v-icon>
           </v-btn>
+          <!-- Save button. -->
           <v-btn rounded small depressed color="primary">
             <v-icon color="white">mdi-content-save</v-icon>
           </v-btn>
