07a7be617523524494bacc877984ebac1287a1bd
kebasyaty <kebasyaty@gmail.com>
2021-01-19 14:05:05 +0200

    Update navigation-drawer

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <!-- Navigation bar. -->
-    <v-navigation-drawer app permanent>
+    <v-navigation-drawer app v-model="drawer" width="320">
       <!-- Company name and short description. -->
       <v-list-item>
         <v-list-item-content>
@@ -15,8 +15,10 @@
 
     <!-- Top panel with control buttons. -->
     <v-app-bar app dense fixed>
+      <!-- Menu button. -->
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <!-- Home button. -->
-      <v-tooltip right>
+      <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
           <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-home</v-icon>
@@ -25,7 +27,7 @@
         <span>Home page</span>
       </v-tooltip>
       <!-- Logout button. -->
-      <v-tooltip right>
+      <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
           <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-logout</v-icon>
@@ -35,7 +37,7 @@
       </v-tooltip>
       <v-spacer></v-spacer>
       <!-- Light or dark theme button. -->
-      <v-tooltip left>
+      <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
           <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-brightness-4</v-icon>
@@ -49,56 +51,6 @@
     <v-main>
       <router-view></router-view>
     </v-main>
-
-    <!-- Footer and control buttons. -->
-    <v-footer fixed class="pl-footer pr-0" color="transparent">
-      <v-card flat tile width="100%">
-        <v-divider></v-divider>
-        <v-footer color="transparent" class="pb-0">
-          <!-- Delete button. -->
-          <v-tooltip top>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn rounded small depressed color="error" v-bind="attrs" v-on="on">
-                <v-icon color="white">mdi-close-thick</v-icon>
-              </v-btn>
-            </template>
-            <span>Delete document</span>
-          </v-tooltip>
-          <v-spacer></v-spacer>
-          <!-- Save button and create a new document. -->
-          <v-tooltip top>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn rounded small depressed color="success" v-bind="attrs" v-on="on">
-                <v-icon color="white">mdi-content-save</v-icon>
-                <v-icon color="white">mdi-ampersand</v-icon>
-                <v-icon color="white">mdi-file-outline</v-icon>
-              </v-btn>
-            </template>
-            <span>Save and create a new document</span>
-          </v-tooltip>
-          <!-- Save button and continue editing the document. -->
-          <v-tooltip top>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn rounded small depressed color="warning" class="mx-4" v-bind="attrs" v-on="on">
-                <v-icon color="white">mdi-content-save</v-icon>
-                <v-icon color="white">mdi-ampersand</v-icon>
-                <v-icon color="white">mdi-file-document-edit-outline</v-icon>
-              </v-btn>
-            </template>
-            <span>Save and continue editing the document</span>
-          </v-tooltip>
-          <!-- Save button. -->
-          <v-tooltip top>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn rounded small depressed color="primary" v-bind="attrs" v-on="on">
-                <v-icon color="white">mdi-content-save</v-icon>
-              </v-btn>
-            </template>
-            <span>Save document</span>
-          </v-tooltip>
-        </v-footer>
-      </v-card>
-    </v-footer>
   </v-app>
 </template>
 
@@ -107,10 +59,12 @@
 export default {
   name: 'App',
 
-  components: {},
+  components: {
+    //
+  },
 
   data: () => ({
-    //
+    drawer: null
   })
 }
 </script>
---
+++ admin-panel/src/views/Footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- Footer and control buttons. -->
+  <v-footer fixed class="pl-footer pr-0" color="transparent">
+    <v-card flat tile width="100%">
+      <v-divider></v-divider>
+      <v-footer color="transparent" class="pb-0">
+        <!-- Delete button. -->
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn rounded small depressed color="error" v-bind="attrs" v-on="on">
+              <v-icon color="white">mdi-close-thick</v-icon>
+            </v-btn>
+          </template>
+          <span>Delete document</span>
+        </v-tooltip>
+        <v-spacer></v-spacer>
+        <!-- Save button and create a new document. -->
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn rounded small depressed color="success" v-bind="attrs" v-on="on">
+              <v-icon color="white">mdi-content-save</v-icon>
+              <v-icon color="white">mdi-ampersand</v-icon>
+              <v-icon color="white">mdi-file-outline</v-icon>
+            </v-btn>
+          </template>
+          <span>Save and create a new document</span>
+        </v-tooltip>
+        <!-- Save button and continue editing the document. -->
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn rounded small depressed color="warning" class="mx-4" v-bind="attrs" v-on="on">
+              <v-icon color="white">mdi-content-save</v-icon>
+              <v-icon color="white">mdi-ampersand</v-icon>
+              <v-icon color="white">mdi-file-document-edit-outline</v-icon>
+            </v-btn>
+          </template>
+          <span>Save and continue editing the document</span>
+        </v-tooltip>
+        <!-- Save button. -->
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn rounded small depressed color="primary" v-bind="attrs" v-on="on">
+              <v-icon color="white">mdi-content-save</v-icon>
+            </v-btn>
+          </template>
+          <span>Save document</span>
+        </v-tooltip>
+      </v-footer>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  components: {
+    //
+  }
+}
+</script>
