f6257802f28168b38d566735fcb47bba49fa4316
kebasyaty <kebasyaty@gmail.com>
2021-01-21 09:39:04 +0200

    Add 404 page

--- admin-panel/src/router/index.js
+++ admin-panel/src/router/index.js
@@ -9,6 +9,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/*',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
--- admin-panel/src/views/Home.vue
+++ admin-panel/src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
+      <v-col cols="12" class="mt-16 mb-5">
+        <v-img :src="require('../assets/logo.svg')" contain height="200" />
       </v-col>
 
-      <v-col>
+      <v-col cols="12">
         <h1 class="text-h2 font-weight-bold mb-3">Welcome, root.</h1>
         <p class="text-h5 font-weight-regular">Good luck on your business!</p>
       </v-col>
