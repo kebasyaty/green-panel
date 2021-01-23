007af5c2f9c90f7b27b569f77fa34d126921e775
kebasyaty <kebasyaty@gmail.com>
2021-01-23 16:12:55 +0200

    Add documents search

--- admin-panel/src/views/DocumentList.vue
+++ admin-panel/src/views/DocumentList.vue
@@ -3,6 +3,23 @@
     <v-card>
       <v-card-title>{{ collectionTitle }}</v-card-title>
       <v-card-subtitle class="pb-0">{{ breadcrumbs }}</v-card-subtitle>
+      <v-card-text class="pt-4">
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              rounded
+              outlined
+              dense
+              hide-details
+              clearable
+              v-model="searchQuery"
+              placeholder="Search"
+              append-icon="mdi-magnify"
+              @click:append="docSearch"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -12,6 +29,11 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'DocumentList',
+
+  data: () => ({
+    // Search query.
+    searchQuery: null
+  }),
 
   computed: {
     ...mapState([
@@ -27,6 +49,15 @@ export default {
       const indexCollection = this.$route.params.indexCollection
       const service = this.serviceList[indexService]
       return `${service.service.title} > ${service.collections[indexCollection].title}`
+    }
+  },
+
+  methods: {
+    // Documents search.
+    docSearch: function () {
+      if (this.searchQuery !== null) {
+        window.console.log(this.searchQuery)
+      }
     }
   }
 }
