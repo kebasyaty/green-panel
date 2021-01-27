d7d619cb55278a90dd746a7178bfc4a0ec11406e
kebasyaty <kebasyaty@gmail.com>
2021-01-27 11:55:27 +0200

    Update Form

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container>
     <v-card class="mt-1">
       <v-toolbar dense flat>
         <v-btn icon @click="goBack">
@@ -8,9 +8,63 @@
       </v-toolbar>
       <v-card-title class="pt-0">{{ docTitle }}</v-card-title>
       <v-card-subtitle>{{ breadcrumbs }}</v-card-subtitle>
-      <v-card-text class="pt-4">
-        <!-- -->
-      </v-card-text>
+      <form>
+        <v-card-text class="mt-2 pa-2">
+          <!-- -->
+        </v-card-text>
+        <v-card-actions class="pa-4">
+          <!-- Delete button. -->
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark outlined small color="red" v-bind="attrs" v-on="on">
+                <v-icon>mdi-close-thick</v-icon>
+              </v-btn>
+            </template>
+            <span>Delete document</span>
+          </v-tooltip>
+          <v-spacer></v-spacer>
+          <!-- Save button and create a new document. -->
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark rounded outlined small color="green" v-bind="attrs" v-on="on">
+                <v-icon>mdi-content-save</v-icon>
+                <v-icon>mdi-ampersand</v-icon>
+                <v-icon>mdi-file-outline</v-icon>
+              </v-btn>
+            </template>
+            <span>Save and create a new document</span>
+          </v-tooltip>
+          <!-- Save button and continue editing the document. -->
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                dark
+                rounded
+                outlined
+                small
+                color="orange"
+                class="mx-4"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-content-save</v-icon>
+                <v-icon>mdi-ampersand</v-icon>
+                <v-icon>mdi-file-document-edit-outline</v-icon>
+              </v-btn>
+            </template>
+            <span>Save and continue editing the document</span>
+          </v-tooltip>
+          <!-- Save button. -->
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark rounded outlined small color="blue" v-bind="attrs" v-on="on">
+                <v-icon>mdi-content-save</v-icon>
+              </v-btn>
+            </template>
+            <span>Save document</span>
+          </v-tooltip>
+        </v-card-actions>
+      </form>
     </v-card>
   </v-container>
 </template>
