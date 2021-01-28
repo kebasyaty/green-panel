5db7081ed765f012fcdc4993b1b7134380c362a2
kebasyaty <kebasyaty@gmail.com>
2021-01-28 15:43:01 +0200

    Update

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -25,13 +25,15 @@
               <v-card-text>
                 <v-card-title class="pa-0 text-subtitle-1 font-weight-medium">{{ field.label }}</v-card-title>
                 <v-card-subtitle class="pl-0 pb-1 pt-3">{{ field.hint }}</v-card-subtitle>
+
+                <!-- Text fields -->
                 <v-text-field
                   dense
                   shaped
                   clearable
                   hide-details
                   v-model="models[field.name]"
-                  v-if="['inputText', 'inputColor', 'inputDate', 'inputDateTime'].includes(field.widget)"
+                  v-if="['inputText', 'inputColor'].includes(field.widget)"
                   :id="field.id"
                   :type="field.input_type"
                   :name="field.name"
@@ -40,6 +42,39 @@
                   :readonly="field.readonly"
                   :class="field.css_classes"
                 ></v-text-field>
+
+                <!-- Date fields -->
+                <v-menu
+                  v-model="menu[field.name]"
+                  v-if="['inputDate'].includes(field.widget)"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      dense
+                      shaped
+                      clearable
+                      hide-details
+                      prepend-icon="mdi-calendar"
+                      v-model="models[field.name]"
+                      :id="field.id"
+                      :type="field.input_type"
+                      :name="field.name"
+                      :placeholder="field.placeholder"
+                      :disabled="field.disabled"
+                      readonly
+                      :class="field.css_classes"
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="models[field.name]" @input="menu[field.name] = false"></v-date-picker>
+                </v-menu>
+
                 <v-card-subtitle
                   v-if="field.warning.length > 0"
                   class="pl-0 pb-0 pt-1 warning--text"
@@ -54,7 +89,7 @@
         </template>
       </v-card-text>
       <!-- Form control buttons. -->
-      <v-card-actions class="pt-6 px-4 pb-4">
+      <v-card-actions class="pt-8 px-4 pb-4">
         <!-- Delete button. -->
         <v-tooltip top>
           <template v-slot:activator="{ on, attrs }">
@@ -108,12 +143,15 @@ export default {
   name: 'DocumentForm',
 
   data: () => ({
+    date: new Date().toISOString().substr(0, 10),
+    datetime: new Date().toISOString().substr(0, 16),
+    menu: { field_date: false, field_datetime: false },
     models: { field_text: 'Lorem ipsum dolor sit amet', field_color: '#3BE40C', field_date: '', field_datetime: '' },
     fields: [
       { widget: 'inputText', id: 'id-field-name', label: 'Label Text', input_type: 'text', name: 'field_text', value: 'Lorem ipsum dolor sit amet', placeholder: 'Enter text', disabled: false, readonly: false, css_classes: 'class-name', hint: 'Quisque tristique magna tortor.', warning: 'Praesent in ligula maximus, viverra nulla sed, aliquam est.', error: 'Pellentesque sit amet lorem sed leo pharetra pretium.', common_msg: 'Proin dolor nibh, imperdiet in odio ac, porttitor blandit ipsum. Etiam sit amet porttitor sapien.' },
       { widget: 'inputColor', id: 'id-field-name-2', label: 'Label Color', input_type: 'color', name: 'field_color', value: '#3BE40C', placeholder: 'Enter color', disabled: false, readonly: false, css_classes: 'class-name', hint: 'Quisque tristique magna tortor.', warning: '', error: '', common_msg: '' },
-      { widget: 'inputDate', id: 'id-field-name-3', label: 'Label Date', input_type: 'date', name: 'field_date', value: '', placeholder: 'Enter date', disabled: false, readonly: false, css_classes: 'class-name', hint: 'Quisque tristique magna tortor.', warning: '', error: '', common_msg: '' },
-      { widget: 'inputDateTime', id: 'id-field-name-4', label: 'Label DateTime', input_type: 'datetime', name: 'field_datetime', value: '', placeholder: 'Enter date and time', disabled: false, readonly: false, css_classes: 'class-name', hint: 'Quisque tristique magna tortor.', warning: '', error: '', common_msg: '' }
+      { widget: 'inputDate', id: 'id-field-name-3', label: 'Label Date', input_type: 'date', name: 'field_date', value: '', placeholder: 'Enter date', disabled: false, readonly: false, css_classes: 'class-name', hint: 'Quisque tristique magna tortor.', warning: '', error: '', common_msg: '', min: '', max: '' },
+      { widget: 'inputDateTime', id: 'id-field-name-4', label: 'Label DateTime', input_type: 'datetime', name: 'field_datetime', value: '', placeholder: 'Enter date and time', disabled: false, readonly: false, css_classes: 'class-name', hint: 'Quisque tristique magna tortor.', warning: '', error: '', common_msg: '', min: '', max: '' }
     ]
   }),
 
