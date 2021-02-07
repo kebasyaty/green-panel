08deb73a4b8f1c5f274cce949e7324c37f460263
kebasyaty <kebasyaty@gmail.com>
2021-02-07 12:51:03 +0200

    Add address for `Admin service`

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,7 @@ async fn main() -> std::io::Result<()> {
             .route("/sitemap.xml", web::route().to(specific::sitemap))
             // ... <- Other services
             // Admin service
+            .route("/admin", web::route().to(services::admin::admin_panel))
             .service(web::scope("/admin").configure(services::admin::config))
             // Primal service (Home page)
             .service(web::scope("/*").configure(services::primal::config))
