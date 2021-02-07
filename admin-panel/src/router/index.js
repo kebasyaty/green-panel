a2a532ccb8f82d3d6089caef694e1038944ccb40
kebasyaty <kebasyaty@gmail.com>
2021-02-07 20:03:49 +0200

    Update

--- admin-panel/src/router/index.js
+++ admin-panel/src/router/index.js
@@ -51,7 +51,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? '/admin/' : '/',
   routes
 })
 
