10a8a71ccfa8a947caa734774ad4573275990ee8
kebasyaty <kebasyaty@gmail.com>
2021-02-13 09:31:05 +0200

    Add comment

--- src/settings/models.rs
+++ src/settings/models.rs
@@ -1,5 +1,11 @@
 //! # Mango-ORM model settings
 //!
+//! `admin` - Service/Sub-Application for user profile management.
+//!
+
+/*
+Unique project key: yQN_1sxRawj2v3Ea
+*/
 
 // KEYWORD it is recommended not to change.
 // Valid characters: _ a-z A-Z 0-9
@@ -7,10 +13,10 @@
 // Example: "PROJECT_NAME_7rzg_cfqQB3B7q7T"
 pub const KEYWORD: &str = "ADMIN_yQN_1sxRawj2v3Ea";
 
-// For a test application.
+// Service/Sub-Application for user profile management.
 pub mod admin {
     pub const SERVICE_NAME: &str = "admin";
-    pub const DATABASE_NAME: &str = "user_yQN_1sxRawj2v3Ea";
+    pub const DATABASE_NAME: &str = "users_yQN_1sxRawj2v3Ea";
     pub const DB_CLIENT_NAME: &str = "default";
     pub const DB_QUERY_DOCS_LIMIT: u32 = 1000;
 }
