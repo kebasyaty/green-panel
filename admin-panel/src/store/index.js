e9d95a6670cd81dabd646ace0f623739833b212a
kebasyaty <kebasyaty@gmail.com>
2021-02-18 14:39:49 +0200

    Update

--- admin-panel/src/store/index.js
+++ admin-panel/src/store/index.js
@@ -15,34 +15,10 @@ export default new Vuex.Store({
     openPanelServiceList: null,
     // Panel width for list of services (Drawer).
     panelWidthServiceList: 360,
-    // List of services (applications) with nested list of collections.
+    //
     selectedService: [undefined, undefined, undefined],
-    serviceList: [
-      {
-        service: { title: 'Название сервиса 1', icon: 'laptop' },
-        collections: [
-          { title: 'Название коллекции 1.1', model_key: '1' },
-          { title: 'Collection name 1.2', model_key: '2' },
-          { title: 'Collection name 1.3', model_key: '3' }
-        ]
-      },
-      {
-        service: { title: 'Service name 2', icon: 'cellphone' },
-        collections: [
-          { title: 'Collection name 2.1', model_key: '4' },
-          { title: 'Collection name 2.2', model_key: '5' },
-          { title: 'Collection name 2.3', model_key: '6' }
-        ]
-      },
-      {
-        service: { title: 'Service name 3', icon: 'wifi' },
-        collections: [
-          { title: 'Collection name 3.1', model_key: '7' },
-          { title: 'Collection name 3.2', model_key: '8' },
-          { title: 'Collection name 3.3', model_key: '9' }
-        ]
-      }
-    ]
+    // List of services (sub-applications) with nested list of collections.
+    serviceList: []
   },
 
   mutations,
