f079101e0f1d0a55a404f8c00ddba4b1f7e74333
kebasyaty <kebasyaty@gmail.com>
2021-02-19 12:32:39 +0200

    Update

--- src/models/registration/admin_panel.rs
+++ src/models/registration/admin_panel.rs
@@ -7,10 +7,11 @@ use serde_json::{json, Value};
 use crate::models::services::admin::users;
 
 // Register models.
+// Hint: get icon names - https://materialdesignicons.com/
 pub fn service_list() -> Value {
     json!([
         {
-            "service": {"title": "Users", "icon": "laptop"},
+            "service": {"title": "Users", "icon": "account-multiple"},
             "collections": [
                 { "title": "User", "model_key": users::User::key() },
             ]
