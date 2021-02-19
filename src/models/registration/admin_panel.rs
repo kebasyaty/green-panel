97a3368b91141ca2d7cebd85608317177b166244
kebasyaty <kebasyaty@gmail.com>
2021-02-19 12:20:01 +0200

    Update

--- src/models/registration/admin_panel.rs
+++ src/models/registration/admin_panel.rs
@@ -1,4 +1,18 @@
 //! # Registering models for the admin panel.
 //!
 
-use crate::models::services::admin::users;
+use serde_json::{json, Value};
+
+// use crate::models::services::admin::users;
+
+// Register models.
+pub fn service_list() -> Value {
+    json!([
+        {
+            "service": {"title": "Название сервиса 1", "icon": "laptop"},
+            "collections": [
+                { "title": "Название коллекции 1.1", "model_key": "1" },
+            ]
+        },
+    ])
+}
