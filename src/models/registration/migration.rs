e4f78c4ac51c5661250563d6a17cc51e4803dd2d
kebasyaty <kebasyaty@gmail.com>
2021-02-20 08:54:49 +0200

    Update

--- src/models/registration/migration.rs
+++ src/models/registration/migration.rs
@@ -5,6 +5,7 @@ use crate::models::{services::admin::users, settings};
 use mango_orm::{Monitor, ToModel, DB_MAP_CLIENT_NAMES};
 
 // Migration Service `Mango`.
+// *************************************************************************************************
 pub fn mango_migration() -> Result<(), Box<dyn std::error::Error>> {
     // Caching MongoDB clients.
     DB_MAP_CLIENT_NAMES.lock()?.insert(
@@ -16,7 +17,7 @@ pub fn mango_migration() -> Result<(), Box<dyn std::error::Error>> {
         project_name: settings::PROJECT_NAME,
         unique_project_key: settings::UNIQUE_PROJECT_KEY,
         // Register models.
-        // ^ ^ ^ -----------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------
         models: vec![users::User::meta()?],
     };
     monitor.migrat();
