3dc73105255032c3da10d3d6a5cfe4f496476a59
kebasyaty <kebasyaty@gmail.com>
2021-02-22 14:14:49 +0200

    Update

--- admin-panel/src/components/NavigationBar.vue
+++ admin-panel/src/components/NavigationBar.vue
@@ -44,7 +44,7 @@
                 v-for="(collection, indexCollection) in item.collections"
                 :key="collection.model_key"
                 class="px-1"
-                @click="[ajaxGetDocumentList(), resetPreActivatedService(indexService)]"
+                @click="[ajaxGetDocumentList({indexService, indexCollection}), resetPreActivatedService(indexService)]"
                 :to="createUrlDocumentList(item.service.title, collection.title, indexService, indexCollection)"
               >
                 <v-list-item-icon class="mr-2">
--- admin-panel/src/store/modules/document_list.js
+++ admin-panel/src/store/modules/document_list.js
@@ -23,18 +23,23 @@ export default {
 
   actions: {
     // Get a list of documents.
-    async ajaxGetDocumentList({ state, commit, rootState }) {
+    async ajaxGetDocumentList({ state, commit, rootState }, payload = {}) {
       if (!state.blockLoadDocs && rootState.serviceList.length > 0) {
         commit('setBlockLoadDocs', true)
-        const collection = rootState.serviceList[router.currentRoute.params.indexService]
-          .collections[router.currentRoute.params.indexCollection]
-        const payload = {
+        let collection
+        if (Object.keys(payload).length > 0) {
+          collection = rootState.serviceList[payload.indexService]
+            .collections[payload.indexCollection]
+        } else {
+          collection = rootState.serviceList[router.currentRoute.params.indexService]
+            .collections[router.currentRoute.params.indexCollection]
+        }
+        Vue.axios.get('/admin/document-list', {
           params: {
             model_key: collection.model_key,
             field_name: collection.doc_name.field
           }
-        }
-        Vue.axios.get('/admin/document-list', payload)
+        })
           .then(response => {
             const data = response.data
             if (data.documents.length > 0) {
