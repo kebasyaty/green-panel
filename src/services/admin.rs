d7c88c757f5a7855d237d0cedd425048f37cd829
kebasyaty <kebasyaty@gmail.com>
2021-03-12 12:30:43 +0200

    Update

--- src/services/admin.rs
+++ src/services/admin.rs
@@ -252,12 +252,10 @@ pub mod request_handlers {
             documents.push(json!({
                 "title": doc.get_str(query.field_name.as_str()).unwrap(),
                 "hash": doc.get_object_id("_id").unwrap().to_hex(),
-                "created_at": mongodb::bson::Bson::String(
-                    doc.get_datetime("created_at").unwrap().to_rfc3339()[..16].into(),
-                ),
-                "updated_at": mongodb::bson::Bson::String(
-                    doc.get_datetime("updated_at").unwrap().to_rfc3339()[..16].into(),
-                )
+                "created_at":
+                    doc.get_datetime("created_at").unwrap().to_rfc3339()[..16].to_string(),
+                "updated_at":
+                    doc.get_datetime("updated_at").unwrap().to_rfc3339()[..16].to_string()
             }))
         }
 
