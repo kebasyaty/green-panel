f4e5664d2b3c10c7f0b8e40a89ad338fd25dc04f
kebasyaty <kebasyaty@gmail.com>
2021-03-14 06:42:47 +0200

    Update

--- admin-panel/src/i18n/lang/en.js
+++ admin-panel/src/i18n/lang/en.js
@@ -29,7 +29,7 @@ export default {
     25: 'Create a new document',
     26: 'New document',
     27: 'Access denied',
-    28: 'Access allowed',
+    28: '',
     29: 'Created',
     30: 'Updated'
   }
