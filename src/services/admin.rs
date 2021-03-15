db244e939f591dee6c87965ac2d81f511c162bfe
kebasyaty <kebasyaty@gmail.com>
2021-03-15 13:28:29 +0200

    Update

--- src/services/admin.rs
+++ src/services/admin.rs
@@ -335,4 +335,33 @@ pub mod request_handlers {
                     "is_authenticated": is_authenticated,
                     "msg_err": msg_err })))
     }
+
+    // Save document
+    // *********************************************************************************************
+        pub async fn save_document(
+        session: Session,
+        query: web::Json<DocQuery>,
+    ) -> Result<HttpResponse, Error> {
+        let mut is_authenticated = false;
+        let mut msg_err = String::new();
+
+        // Access request identity
+        // -----------------------------------------------------------------------------------------
+        if session.get::<String>("user")?.is_some()
+            && session.get::<bool>("is_active")?.unwrap()
+            && session.get::<bool>("is_staff")?.unwrap()
+        {
+            is_authenticated = true;
+        } else {
+            msg_err = "Authentication failed.".to_string();
+        }
+
+                // Return json response
+        // -----------------------------------------------------------------------------------------
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .json(json!({ "document": document,
+                    "is_authenticated": is_authenticated,
+                    "msg_err": msg_err })))
+    }
 }
