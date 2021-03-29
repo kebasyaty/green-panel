38dce0af8e6aa5089dbdb451bd8b90c496cdd051
kebasyaty <kebasyaty@gmail.com>
2021-03-29 12:25:27 +0300

    Update

--- admin-panel/src/components/PopUpMsgs.vue
+++ admin-panel/src/components/PopUpMsgs.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Pop-up messages -->
-  <v-snackbar app top text timeout="-1" :color="msgStatus" v-model="compShowMsg">
-    <v-alert text outlined :color="msgStatus" icon="mdi-fire" border="left">{{ textMsg }}</v-alert>
+  <v-snackbar app top text outlined timeout="-1" :color="msgStatus" v-model="compShowMsg">
+    <span v-html="textMsg"></span>
     <template v-slot:action="{ attrs }">
       <v-btn icon :color="msgStatus" v-bind="attrs" @click="compShowMsg = false">
         <v-icon>mdi-close</v-icon>
