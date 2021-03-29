ee5fbb672aa3bba10a45fbb97d59e72c429b4403
kebasyaty <kebasyaty@gmail.com>
2021-03-29 18:54:59 +0300

    Update

--- admin-panel/src/views/DocumentList.vue
+++ admin-panel/src/views/DocumentList.vue
@@ -251,7 +251,6 @@ export default {
     // Delete selected documents.
     deleteDocs: function () {
       this.setShowMsg(false)
-      this.setSearchQuery(null)
       this.runShowOverlayPageLockout(true)
       const indexService = this.$route.params.indexService
       const indexCollection = this.$route.params.indexCollection
