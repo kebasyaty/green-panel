9418ff136fa5be5277ab240d5bc4173c0dfcf260
kebasyaty <kebasyaty@gmail.com>
2021-03-30 14:45:04 +0300

    Update

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -1408,7 +1408,7 @@ export default {
           if (!data.is_authenticated) {
             this.setIsAuthenticated(false)
           } else if (data.msg_err.length === 0) {
-            //
+            this.fieldData[fieldName].options.concat(this.currValDynItem)
           } else {
             console.log(data.msg_err)
             this.runShowMsg({ text: data.msg_err, status: 'error' })
