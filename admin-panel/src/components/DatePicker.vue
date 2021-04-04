027c94ee7d0c0b9e2c62274e3718bbacb2e0ca27
kebasyaty <kebasyaty@gmail.com>
2021-04-04 11:56:07 +0300

    Update

---
+++ admin-panel/src/components/DatePicker.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-date-picker
+      scrollable
+      v-model="data"
+      year-icon="mdi-calendar-blank"
+      color="primary"
+      :min="min"
+      :max="max"
+      :disabled="disabled"
+      :readonly="readonly"
+      :locale="$i18n.locale"
+    >
+      <v-spacer></v-spacer>
+      <v-btn text small color="primary" @click="vMenu[field.name] = false">{{ $t('message.18') }}</v-btn>
+      <v-btn
+        text
+        small
+        color="primary"
+        @click="$refs[field.name][0].save(fieldsData[field.name])"
+      >OK</v-btn>
+    </v-date-picker>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DatePicker',
+
+  props: {
+    data: {
+      type: String,
+      default: new Date().toISOString().substr(0, 10)
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    min: {
+      type: String,
+      default: ''
+    },
+    max: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
