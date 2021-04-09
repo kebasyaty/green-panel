d0064d87f5060b108893878d5071119f4e85b41d
kebasyaty <kebasyaty@gmail.com>
2021-04-09 10:27:51 +0300

    update

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -930,10 +930,22 @@ import { mapState, mapMutations, mapActions } from 'vuex'
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 // Plugins.
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter'
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage'
+import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Image from '@ckeditor/ckeditor5-image/src/image'
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption'
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle'
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar'
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload'
 import Link from '@ckeditor/ckeditor5-link/src/link'
+import List from '@ckeditor/ckeditor5-list/src/list'
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'
 
 export default {
   name: 'DocumentForm',
@@ -954,10 +966,22 @@ export default {
       language: 'ru',
       plugins: [
         Essentials,
+        UploadAdapter,
+        Autoformat,
         Bold,
         Italic,
+        BlockQuote,
+        EasyImage,
+        Heading,
+        Image,
+        ImageCaption,
+        ImageStyle,
+        ImageToolbar,
+        ImageUpload,
         Link,
-        Paragraph
+        List,
+        Paragraph,
+        Alignment
       ],
       toolbar: [
         { name: 'document', groups: ['mode', 'document', 'doctools'], items: ['Source', '-', 'Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates'] },
