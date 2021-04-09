41c3388c90db77b33e98bcc862a0b1db81219375
kebasyaty <kebasyaty@gmail.com>
2021-04-09 08:13:16 +0300

    update

--- admin-panel/vue.config.js
+++ admin-panel/vue.config.js
@@ -16,6 +16,7 @@ module.exports = {
       new CKEditorWebpackPlugin({
         // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
         language: 'en',
+        additionalLanguages: 'all',
         // Append translations to the file matching the `app` name.
         translationsOutputFile: /app/
       })
