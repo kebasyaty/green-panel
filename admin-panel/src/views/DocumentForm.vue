35f66fdf979f04d56d1bf0a5c380408408c923a4
kebasyaty <kebasyaty@gmail.com>
2021-04-09 08:36:44 +0300

    Update

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -928,8 +928,6 @@ import { mapState, mapMutations, mapActions } from 'vuex'
 
 // CKEditor 5.
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
-// Translations.
-// import '@ckeditor/ckeditor5-build-classic/build/translations/ru'
 // Plugins.
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials'
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold'
