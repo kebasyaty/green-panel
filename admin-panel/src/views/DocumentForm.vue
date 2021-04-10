35f981350766ae652de9c168790dcbf5c25cafad
kebasyaty <kebasyaty@gmail.com>
2021-04-10 10:41:02 +0300

    Update

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -1787,7 +1787,7 @@ export default {
 
   created() {
     //
-    // this.editorConfig.language = 'ru' // this.$i18n.locale
+    this.editorConfig.language = 'ru' // this.$i18n.locale
     // Get document.
     this.getDoc()
   }
