290224bc0bc03de9136fe029ca22bc751be4a0b4
kebasyaty <kebasyaty@gmail.com>
2021-04-12 11:12:45 +0300

    Update

--- src/models/registration/admin_panel.rs
+++ src/models/registration/admin_panel.rs
@@ -36,32 +36,32 @@ pub fn config_ckeditor(config_name: &str) -> Value {
         // By default.
         "default" => {
             json!({
-                "toolbar": {
-                    "items": [
-                        "heading", "|",
-                        "textPartLanguage", "|",
-                        "alignment", "|",
-                        "bold", "italic", "underline", "strikethrough", "|",
-                        "subscript", "superscript", "|",
-                        "fontColor", "fontBackgroundColor", "fontFamily", "fontsize", "|",
-                        "bulletedList", "numberedList", "todoList", "|",
-                        "-",
-                        "outdent", "indent", "|",
-                        "blockQuote", "highlight", "|",
-                        "pageBreak", "removeFormat", "selectAll", "|",
-                        "link", "specialCharacters", "insertTable", "mediaEmbed",
-                        "horizontalLine", "code", "codeBlock", "|",
-                        "undo",
-                        "redo"
+                "toolbar": [
+                    "heading", "|",
+                    "textPartLanguage", "|",
+                    "alignment", "|",
+                    "bold", "italic", "underline", "strikethrough", "|",
+                    "subscript", "superscript", "|",
+                    "fontColor", "fontBackgroundColor", "fontFamily", "fontsize", "|",
+                    "bulletedList", "numberedList", "todoList", "|",
+                    "-",
+                    "outdent", "indent", "|",
+                    "blockQuote", "highlight", "|",
+                    "pageBreak", "removeFormat", "selectAll", "|",
+                    "link", "specialCharacters", "insertTable", "mediaEmbed",
+                    "horizontalLine", "code", "codeBlock", "|",
+                    "undo",
+                    "redo"
+                ],
+                "table": {
+                    "contentToolbar": [
+                        "tableColumn", "tableRow", "mergeTableCells",
+                        "tableProperties", "tableCellProperties"
                     ],
-                    "table": {
-                        "contentToolbar": [
-                            "tableColumn", "tableRow", "mergeTableCells",
-                            "tableProperties", "tableCellProperties"
-                        ]
-                    },
-                    "shouldNotGroupWhenFull": true
-                }
+                    "tableProperties": {},
+                    "tableCellProperties": {}
+                },
+                "shouldNotGroupWhenFull": true
             })
         }
         // Error
