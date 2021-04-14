56701f9631c37363be99f1b699937d642ca09fba
kebasyaty <kebasyaty@gmail.com>
2021-04-14 15:25:16 +0300

    Update

--- admin-panel/src/store/index.js
+++ admin-panel/src/store/index.js
@@ -8,6 +8,101 @@ import overlays from '@/store/modules/overlays'
 
 Vue.use(Vuex)
 
+const customColorPalette = [
+  {
+    color: '#F44336',
+    label: 'Red'
+  },
+  {
+    color: '#E91E63',
+    label: 'Pink'
+  },
+  {
+    color: '#9C27B0',
+    label: 'Purple'
+  },
+  {
+    color: '#673AB7',
+    label: 'Deep Purple'
+  },
+  {
+    color: '#3F51B5',
+    label: 'Indigo'
+  },
+  {
+    color: '#2196F3',
+    label: 'Blue'
+  },
+  {
+    color: '#03A9F4',
+    label: 'Light Blue'
+  },
+  {
+    color: '#00BCD4',
+    label: 'Cyan'
+  },
+  {
+    color: '#009688',
+    label: 'Teal'
+  },
+  {
+    color: '#4CAF50',
+    label: 'Green'
+  },
+  {
+    color: '#8BC34A',
+    label: 'Light Green'
+  },
+  {
+    color: '#CDDC39',
+    label: 'Lime'
+  },
+  {
+    color: '#FFEB3B',
+    label: 'Yellow'
+  },
+  {
+    color: '#FFC107',
+    label: 'Amber'
+  },
+  {
+    color: '#FF9800',
+    label: 'Orange'
+  },
+  {
+    color: '#FF5722',
+    label: 'Deep Orange'
+  },
+  {
+    color: '#795548',
+    label: 'Brown'
+  },
+  {
+    color: '#607D8B',
+    label: 'Blue Grey'
+  },
+  {
+    color: '#000000',
+    label: 'Black'
+  },
+  {
+    color: '#424242',
+    label: 'Dim Grey'
+  },
+  {
+    color: '#9E9E9E',
+    label: 'Grey'
+  },
+  {
+    color: '#E0E0E0',
+    label: 'Light Grey'
+  },
+  {
+    color: '#FFFFFF',
+    label: 'White'
+  }
+]
+
 export default new Vuex.Store({
   state: {
     brand: '',
@@ -51,8 +146,20 @@ export default new Vuex.Store({
           'tableColumn', 'tableRow', 'mergeTableCells',
           'tableProperties', 'tableCellProperties'
         ],
-        tableProperties: {},
-        tableCellProperties: {}
+        tableProperties: {
+          borderColors: customColorPalette,
+          backgroundColors: customColorPalette
+        },
+        tableCellProperties: {
+          borderColors: customColorPalette,
+          backgroundColors: customColorPalette
+        }
+      },
+      fontColor: {
+        colors: customColorPalette
+      },
+      fontBackgroundColor: {
+        colors: customColorPalette
       }
     }
   },
