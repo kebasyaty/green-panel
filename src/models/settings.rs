dfd6fb97201c168a90b3b57cddf33932466323d6
kebasyaty <kebasyaty@gmail.com>
2021-04-17 08:46:16 +0300

    Update

--- src/models/settings.rs
+++ src/models/settings.rs
@@ -25,6 +25,21 @@ pub mod default {
     // Valid characters: _ a-z A-Z 0-9
     // Max size: 31
     // First character: a-z A-Z
+    pub const SERVICE_NAME: &str = "admin";
+    // Valid characters: _ a-z A-Z 0-9
+    // Max size: 21
+    // First character: a-z A-Z
+    pub const DATABASE_NAME: &str = "users";
+    pub const DB_CLIENT_NAME: &str = "default";
+    pub const DB_QUERY_DOCS_LIMIT: u32 = 1000;
+}
+
+// Electric Cars
+// *************************************************************************************************
+pub mod electric_cars {
+    // Valid characters: _ a-z A-Z 0-9
+    // Max size: 31
+    // First character: a-z A-Z
     pub const SERVICE_NAME: &str = "electric_cars";
     // Valid characters: _ a-z A-Z 0-9
     // Max size: 21
