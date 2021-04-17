5dfab556cd49c4b261150423b772d724753fa7e9
kebasyaty <kebasyaty@gmail.com>
2021-04-17 08:26:04 +0300

    Update

--- src/models/settings.rs
+++ src/models/settings.rs
@@ -10,14 +10,14 @@
 // Valid characters: _ a-z A-Z 0-9
 // Max size: 21
 // First character: a-z A-Z
-pub const PROJECT_NAME: &str = "project_name";
+pub const PROJECT_NAME: &str = "store";
 
 // Unique project key.
 // Hint: UNIQUE_PROJECT_KEY it is recommended not to change.
 // Valid characters: a-z A-Z 0-9
 // Size: 8-16
 // Example: "7rzgacfqQB3B7q7T"
-pub const UNIQUE_PROJECT_KEY: &str = "WjsDYzy4VvhHCda9";
+pub const UNIQUE_PROJECT_KEY: &str = "Gm5EKQZRAWvV6BW";
 
 // Default settings.
 // *************************************************************************************************
@@ -25,11 +25,11 @@ pub mod default {
     // Valid characters: _ a-z A-Z 0-9
     // Max size: 31
     // First character: a-z A-Z
-    pub const SERVICE_NAME: &str = "service_name";
+    pub const SERVICE_NAME: &str = "electric_cars";
     // Valid characters: _ a-z A-Z 0-9
     // Max size: 21
     // First character: a-z A-Z
-    pub const DATABASE_NAME: &str = "database_name";
+    pub const DATABASE_NAME: &str = "cars";
     pub const DB_CLIENT_NAME: &str = "default";
     pub const DB_QUERY_DOCS_LIMIT: u32 = 1000;
 }
