8ef72529379cd8b9bf36e84d012063c2f07cbbfb
kebasyaty <kebasyaty@gmail.com>
2021-04-25 16:56:32 +0300

    Update

--- admin-panel/src/views/DocumentList.vue
+++ admin-panel/src/views/DocumentList.vue
@@ -215,7 +215,7 @@ export default {
     currentPageNumber: function (numPage) {
       this.progressionStep = ((this.docsPerPage * (numPage - 1))) + 1
       this.$route.query.page = numPage.toString()
-      history.replaceState(null, '', `${window.location.protocol}//${window.location.host}/admin${this.$route.path}?page=${numPage}`)
+      history.replaceState(null, '', `${window.location.protocol}//${window.location.host}/admin${this.$route.path}?per=${this.docsPerPage}&page=${numPage}`)
     }
   },
 
