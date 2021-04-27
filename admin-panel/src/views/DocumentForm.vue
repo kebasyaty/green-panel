fe4af458d526c20322a4d99a25a436d42d99ae46
kebasyaty <kebasyaty@gmail.com>
2021-04-27 15:32:23 +0300

    Update

--- admin-panel/src/views/DocumentForm.vue
+++ admin-panel/src/views/DocumentForm.vue
@@ -937,16 +937,6 @@
       v-model="dialogDocDelete"
       max-width="290"
     >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Open Dialog
-        </v-btn>
-      </template>
       <v-card>
         <v-card-title class="h6">
            {{ $t('message.39') }}
