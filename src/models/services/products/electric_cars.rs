fa5289f509aa5e89fbd414027cc27203e8de73fa
kebasyaty <kebasyaty@gmail.com>
2021-05-08 16:14:47 +0300

    Update

--- src/models/services/products/electric_cars.rs
+++ src/models/services/products/electric_cars.rs
@@ -72,7 +72,7 @@ pub struct ElectricCar {
     #[field_attrs(
         widget = "selectText",
         label = "Body Type",
-        options = r#"["Convertible", "Coupe", "Crossover", "Hatchback", "Luxury", "MUV", "Notchback", "Sedan", "Sport Car", "SUV", "Wagon", "Super Car", "Van / Minivan"]"#,
+        options = r#"["Convertible", "Coupe", "Crossover", "Hatchback", "Luxury", "MUV", "Notchback", "Sedan", "Sport Car", "SUV", "Wagon", "Super Car", "Van/Minivan", "Buggy", "Buggy/Golf Carts"]"#,
         hint = "Classification of cars on the basis of body type"
     )]
     pub body_type: Option<String>,
